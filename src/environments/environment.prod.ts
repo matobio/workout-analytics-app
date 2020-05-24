b7cd77759c4a0ee122e744f7a544cedcbcfcd125
unknown <teymanu@gmail.com>
2020-05-24 16:43:00 +0200

    commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: FIREBASE_CONFIG.apiKey,
+        authDomain: FIREBASE_CONFIG.authDomain,
+        databaseURL: FIREBASE_CONFIG.databaseURL,
+        projectId: FIREBASE_CONFIG.projectId,
+        storageBucket: FIREBASE_CONFIG.storageBucket,
+        messagingSenderId: FIREBASE_CONFIG.messagingSenderId,
+        appId: FIREBASE_CONFIG.appId
+    }
+};
