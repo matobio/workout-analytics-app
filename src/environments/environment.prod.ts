5da0eed9a3fe59be86b2b2a416e960e3b28f5c95
unknown <teymanu@gmail.com>
2020-05-24 16:38:16 +0200

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCeVH2U-j5J_oyEXEmsCEV8de7X5ia9KyE",
-    authDomain: "fir-test-4ad37.firebaseapp.com",
-    databaseURL: "https://fir-test-4ad37.firebaseio.com",
-    projectId: "fir-test-4ad37",
-    storageBucket: "fir-test-4ad37.appspot.com",
-    messagingSenderId: "699626051514",
-    appId: "1:699626051514:web:707cfe99ed34f0f930ef83",
-    measurementId: "G-F3E0H5FJ59"
+    apiKey: FIREBASE_CONFIG.apiKey,
+    authDomain: FIREBASE_CONFIG.authDomain,
+    databaseURL: FIREBASE_CONFIG.databaseURL,
+    projectId: FIREBASE_CONFIG.projectId,
+    storageBucket: FIREBASE_CONFIG.storageBucket,
+    messagingSenderId: FIREBASE_CONFIG.messagingSenderId,
+    appId: FIREBASE_CONFIG.appId
   }
 };
